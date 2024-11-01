09d1030c86379a991b417bd7cde2c7f3f7e6a222
Andrei Vorobev <andrey.vorob1995@gmail.com>
2024-11-01 18:15:31 -0400

    Update build.sh script to install TA-Lib in a user-writable directory

--- build.sh
+++ build.sh
@@ -3,17 +3,24 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 set -x  # Print commands and their arguments as they are executed
 
-# Install dependencies using wget and tar
+# Define the installation directory
+INSTALL_DIR=$HOME/ta-lib
+
+# Download and install TA-Lib
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib
-./configure --prefix=/usr
+./configure --prefix=$INSTALL_DIR
 make
 make install
 
 # Clean up
 cd ..
 rm -rf ta-lib ta-lib-0.4.0-src.tar.gz
+
+# Set environment variables to use the installed TA-Lib
+export CFLAGS="-I$INSTALL_DIR/include"
+export LDFLAGS="-L$INSTALL_DIR/lib"
 
 # Install Python dependencies
 pip install -r requirements.txt
