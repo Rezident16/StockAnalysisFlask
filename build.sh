911c3518d580e322936fa501a20250bc11c47d4a
Andrei Vorobev <andrey.vorob1995@gmail.com>
2024-11-01 18:08:46 -0400

    Add build.sh script and update vercel.json

--- build.sh
+++ build.sh
@@ -3,11 +3,7 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 set -x  # Print commands and their arguments as they are executed
 
-# Update package list and install dependencies
-apt-get update
-apt-get install -y build-essential wget
-
-# Download and install TA-Lib
+# Install dependencies using wget and tar
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib
